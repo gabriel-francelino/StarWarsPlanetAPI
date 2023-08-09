9f926130bffbfb52591933d9457ae4c6efc36707
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-08-09 14:48:41 -0300

    Criando testes para verificar dados inválidos

--- src/test/java/com/example/starwarsplanetapi/common/PlanetConstants.java
+++ src/test/java/com/example/starwarsplanetapi/common/PlanetConstants.java
@@ -4,4 +4,5 @@ import com.example.starwarsplanetapi.domain.Planet;
 
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("Tatooine", "arid", "desert");
+    public static final Planet INVALID_PLANET = new Planet("", "", "");
 }
--- src/test/java/com/example/starwarsplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/example/starwarsplanetapi/domain/PlanetServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.starwarsplanetapi.domain;
 
+import org.assertj.core.api.Assertions;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -13,8 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static com.example.starwarsplanetapi.common.PlanetConstants.PLANET;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static com.example.starwarsplanetapi.common.PlanetConstants.INVALID_PLANET;
 
 // @SpringBootTest(classes = PlanetService.class) - Não é muito eficiente quando se coloca vários testes
 @ExtendWith(MockitoExtension.class) // Mais eficiente
@@ -39,4 +39,13 @@ public class PlanetServiceTest {
         // Assert
         MatcherAssert.assertThat(sut, Matchers.equalTo(PLANET));
     }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException(){
+        Mockito.when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+        Assertions.assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
 }
+
+
+

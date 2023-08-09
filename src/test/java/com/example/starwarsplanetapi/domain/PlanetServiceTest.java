583b6e749846a17c052d66308f6a8e55fe26b910
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-08-08 22:59:19 -0300

    Fazendo o mock do repositório para teste

--- src/test/java/com/example/starwarsplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/example/starwarsplanetapi/domain/PlanetServiceTest.java
@@ -1,8 +1,11 @@
 package com.example.starwarsplanetapi.domain;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
 import static com.example.starwarsplanetapi.common.PlanetConstants.PLANET;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -11,10 +14,20 @@ import static org.hamcrest.Matchers.equalTo;
 public class PlanetServiceTest {
     @Autowired
     private PlanetService planetService;
+
+    @MockBean
+    private PlanetRepository planetRepository;
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet(){
+        // princípios AAA(Arrange, Act, Assert)
+        // Arrange
+        Mockito.when(planetRepository.save((PLANET))).thenReturn(PLANET);
+
+        // Act
         // sut - system under test
         Planet sut = planetService.create(PLANET);
+
+        // Assert
         assertThat(sut, equalTo(PLANET));
     }
 }

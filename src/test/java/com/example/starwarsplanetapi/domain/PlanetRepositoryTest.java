e7dc14f24b70ed4a7898bdc5c4d2ffa32a73420d
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-08-11 10:16:12 -0300

    Criando teste de integração com banco de dados

---
+++ src/test/java/com/example/starwarsplanetapi/domain/PlanetRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.starwarsplanetapi.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.example.starwarsplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@SpringBootTest(classes = PlanetRepository.class)
+@DataJpaTest // com essa anotação não será necessário o @SpringBootTest
+public class PlanetRepositoryTest {
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet(){
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+        System.out.println(planet.toString());
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+}

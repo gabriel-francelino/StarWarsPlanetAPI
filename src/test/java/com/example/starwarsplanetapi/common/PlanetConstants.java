779077709ad2b20c480656697ece57d40bf57b5f
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-08-18 16:10:36 -0300

    Criando lista de planetas para testar listagem de planetas do controlador

--- src/test/java/com/example/starwarsplanetapi/common/PlanetConstants.java
+++ src/test/java/com/example/starwarsplanetapi/common/PlanetConstants.java
@@ -1,8 +1,22 @@
 package com.example.starwarsplanetapi.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.starwarsplanetapi.domain.Planet;
 
 public class PlanetConstants {
-    public static final Planet PLANET = new Planet("Tatooine", "arid", "desert");
+    public static final Planet PLANET = new Planet("name", "climate", "terrain");
     public static final Planet INVALID_PLANET = new Planet("", "", "");
+
+    public static final Planet TATOOINE = new Planet(1L, "Tatooine", "arid", "desert");
+    public static final Planet ALDERAAN = new Planet(2L, "Alderaan", "temperate", "grasslands, mountains");
+    public static final Planet YAVINIV = new Planet(3L, "Yavin IV", "temperate, tropical", "jungle, rainforests");
+    public static final List<Planet> PLANETS = new ArrayList<>() {
+        {
+            add(TATOOINE);
+            add(ALDERAAN);
+            add(YAVINIV);
+        }
+    };
 }

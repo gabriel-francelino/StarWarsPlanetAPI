cf201f9410536204a8593e80f60f85319e4467e3
gabriel-francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-08-22 14:01:48 -0300

    Classe para teste subcutâneo criada

---
+++ src/test/java/com/example/starwarsplanetapi/PlanetIT.java
@@ -0,0 +1,14 @@
+package com.example.starwarsplanetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PlanetIT {
+    @Test
+    public void contextLoad(){
+
+    }
+
+}

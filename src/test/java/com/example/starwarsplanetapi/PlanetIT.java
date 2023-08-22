410f0bb92d1285f8bb6576a8514c2186c581a9ff
gabriel-francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-08-22 14:29:47 -0300

    Criando teste para cadastro de usuário com sucesso

--- src/test/java/com/example/starwarsplanetapi/PlanetIT.java
+++ src/test/java/com/example/starwarsplanetapi/PlanetIT.java
@@ -1,14 +1,31 @@
 package com.example.starwarsplanetapi;
 
+import com.example.starwarsplanetapi.domain.Planet;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+
+import static com.example.starwarsplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PlanetIT {
+    @Autowired
+    private TestRestTemplate restTemplate;
     @Test
-    public void contextLoad(){
+    public void createPlanet_returnsCreated(){
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
 
     }
 

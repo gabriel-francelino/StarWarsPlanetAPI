f93a6800e9323a649daf33557c6823caf0491da6
gabriel-francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-08-22 14:11:01 -0300

    Add perfis de teste

--- src/test/java/com/example/starwarsplanetapi/PlanetIT.java
+++ src/test/java/com/example/starwarsplanetapi/PlanetIT.java
@@ -2,8 +2,9 @@ package com.example.starwarsplanetapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-
+@ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PlanetIT {
     @Test
